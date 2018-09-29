144f9c4a3b6f518c1c1df23b5ed13d86bdd59ebd
haileymag <hailey.mag@gmail.com>
2018-09-29 13:28:04 -0700

    open add string

---
+++ steg/steganography/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace steganography
+{
+    public partial class Steganography : Form
+    {
+        public Steganography()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void imageretreival_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void uploadimage_Click(object sender, EventArgs e)
+        {
+                string fileName = null;
+
+                using (OpenFileDialog openFileDialog1 = new OpenFileDialog())
+                {
+                    openFileDialog1.InitialDirectory = "c:\\";
+                    openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                    openFileDialog1.FilterIndex = 2;
+                    openFileDialog1.RestoreDirectory = true;
+
+                    if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                    {
+                        fileName = openFileDialog1.FileName;
+                    }
+                }
+
+                if (fileName != null)
+                {
+                    //Do something with the file, for example read text from it
+                    string text = File.ReadAllText(fileName);
+                }
+            }
+
+        private void Steganography_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+    }
+
